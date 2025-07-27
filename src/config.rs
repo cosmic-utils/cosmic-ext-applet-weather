62d39d3e7f2b8ac169247b2d9781da3bbbb445ed
rwxroot <rwxroot@proton.me>
2025-07-27 23:05:51 +0530

    Add applet GUI to manage coordinates and unit

--- src/config.rs
+++ src/config.rs
@@ -8,10 +8,11 @@ pub const APP_ID: &str = "io.github.cosmic_utils.weather-applet";
 pub const SUN_ICON: &str = "io.github.cosmic_utils.weather-applet-symbolic-sun";
 pub const MOON_ICON: &str = "io.github.cosmic_utils.weather-applet-symbolic-moon";
 
-#[derive(Default, Debug, CosmicConfigEntry)]
+#[derive(Clone, Default, Debug, CosmicConfigEntry)]
 pub struct WeatherConfig {
     pub latitude: f64,
     pub longitude: f64,
+    pub use_fahrenheit: bool,
 }
 
 impl WeatherConfig {
@@ -23,10 +24,25 @@ impl WeatherConfig {
         match Self::config_handler() {
             Some(config_handler) => WeatherConfig::get_entry(&config_handler)
                 .map_err(|error| {
-                    tracing::info!("Error whilst loading config: {:#?}", error);
+                    tracing::error!("Error whilst loading config: {:#?}", error);
                 })
                 .unwrap_or_default(),
             None => WeatherConfig::default(),
         }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Flags {
+    pub config: WeatherConfig,
+    pub config_handler: Option<cosmic_config::Config>,
+}
+
+pub fn flags() -> Flags {
+    let (config, config_handler) = (WeatherConfig::config(), WeatherConfig::config_handler());
+
+    Flags {
+        config,
+        config_handler,
     }
 }
