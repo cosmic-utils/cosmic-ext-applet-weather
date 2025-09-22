39a5b29b3dcdcf179ad0f8cd7488a77e6491d226
rwxroot <rwxroot@proton.me>
2025-09-22 19:19:29 +0530

    Remove flatpak folder and weather-applet.json

--- flatpak/generate-cargo-sources.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate cargo sources for Flatpak
-# This script generates the cargo-sources.json file needed for offline builds
-
-uv run --script flatpak/flatpak-cargo-generator.py Cargo.lock -o cargo-sources.json
